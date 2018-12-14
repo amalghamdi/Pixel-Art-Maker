ec455509aa7e99b2c0de22ba6c441f12d0233c4a
amalghamdi <44754439+amalghamdi@users.noreply.github.com>
2018-12-14 03:17:04 +0300

    Define Variables (height, width)

--- designs.js
+++ designs.js
@@ -3,8 +3,11 @@
 
 // When size is submitted by the user, call makeGrid()
 
+//Define Variables (height, width)
 function makeGrid() {
+  var height = document.getElementById('inputHeight').value;
+  var width = document.getElementById('inputWidth').value;
 
-// Your code goes here!
-
+  
 }
+
